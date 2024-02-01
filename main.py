77d1a1c3230d3e0f84f984e9d4c398d29b27f4a5
Mike <magutierrezsistemas@gmail.com>
2024-02-01 19:49:07 +0100

    storing data

--- main.py
+++ main.py
@@ -2,34 +2,59 @@ from B2 import *
 from collections import defaultdict
 import pandas as pd
 
+def get_info(language_level, num_exam, students):
+        dict_results = {
+             "Nombre" : [],
+             "Reading": [],
+             "Listening": [],
+             "Writting": [],
+             "Speaking": [],
+             "English_use": []
+        }
 
-if __name__ == "__main__":    
+        text = "Escribe los aciertos y el total\n" + "Escribe el primer numero separado por una / y luego el segundo"        
+        for num_exam in range(0,num_exam):
+            for students in range(0,students):
+                print("Nombre del alumno")
+                name = str(input())
+                dict_results['Nombre'].append(name)
+                '''
+                print('Reading')
+                print(text)
+                reading = filter(str(input()))
+                dict_results['Reading'].append(reading)
+                print('Listening')
+                print(text)                
+                listening = filter(str(input()))
+                dict_results['Listening'].append(listening)
+                print('Writting')
+                print(text)
+                writting = filter(str(input()))
+                dict_results['Writting'].append(writting)
+                print('Speaking')
+                print(text)
+                speaking = filter(str(input()))
+                dict_results['Speaking'].append(speaking)
+                if language_level == "B2":
+                    print('English use')
+                    print(text)
+                    english_use = filter(str(input()))
+                    dict_results['English_use'].append(english_use)
+                    '''
+        print(dict_results)
+        return dict_results
+
+
+if __name__ == "__main__":
+    language_level = " "
     print("¿Cuántos alumnos tienes?")
     students = int(input())
-    print("¿Se presentan a B1 o B2?")    
-    languageLevel = str(input())
     print('¿Cuántos exámenes quieres evaluar?')
-    numExam = int(input())
-    if languageLevel != "B1" or languageLevel != "B2":
-        while(languageLevel != "B1" or languageLevel != "B2"):
-            print("¿Se presentan a B1 o B2?")
-            languageLevel = input()
-    else:
-        alumno = { 'Nombre' : '', 'Notas' : [] }
-        for students in range(0,students-1):
-            print("Nombre del alumno")
-            name = str(input())
-            alumno['Nombre'] = name
-            print('Reading')
-            print('Escribe los aciertos y el total')
-            reading = filter(str(input()))
-            print('Listening')
-            print('Escribe los aciertos y el total')
-            listening = filter(str(input()))
-            print('Writting')
-            print('Escribe los aciertos y el total')
-            writting = filter(str(input()))
-            print('Speaking \n =======')
-            print('Escribe los aciertos y el total')
-            speaking = filter(str(input()))
+    num_exam = int(input())
+    while(language_level != "B1" and language_level != "B2"):
+        print("¿Se presentan a B1 o B2?")
+        language_level = str(input())
+        print(language_level)
+    
+    dict_evaluaciones = pd.DataFrame(get_info(language_level,num_exam,students))    
 
