cef99a1307741ecf793c0bdd124de3b9c6edec53
mainformatico <magutierrezsistemas@gmail.com>
2024-09-12 15:13:29 +0200

    storing results for one student into docx file

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ from docx import Document
 import docxedit
 
 
-def get_info(language_level, num_exam, students,average_overall,average_reading,average_listening,average_speaking,average_writing,average_use_english):
+def get_initial_data():
+    language_level = [" "]
+    print("¿Cuántos alumnos tienes?")
+    students = int(input())
+    print('¿Cuántos exámenes quieres evaluar?')
+    num_exam = int(input())
+    while(language_level[0] != "B1" and language_level[0] != "B2" and language_level[0] != "C1"):
+        print("¿Se presentan a B1, B2 o C1?")
+        language_level[0] = str(input())
+        print(language_level[0])
+        if language_level[0] == "B2" or language_level[0] == "C1":
+            print("¿Son alumnos de primer o segundo año? (escribe 1 o 2)")
+            language_level.append(int(input()))
+    
+    return language_level, students, num_exam
+
+
+
+def get_info(average_overall,average_reading,average_listening,average_speaking,average_writing,average_use_english):
+        language_level, students, num_exam = get_initial_data()
+        
         dict_results = {
             'Nombre' : [],
             'Reading': [],
@@ -39,14 +59,14 @@ def get_info(language_level, num_exam, students,average_overall,average_reading,
                 aux = str(input())
                 if not aux:
                     result = 'X'
-                    dict_results['Reading'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Reading'].append(str(aux))
                 else:
                     reading = instance.filter(aux)
                     result = instance.percent_category(int(reading[0]),int(reading[1]))
                 #print(result)
                     average_test.append(result)
                     avg_reading.append(result)
-                    dict_results['Reading'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Reading'].append(str(aux))
                 #dict_results['Reading'].append(str(reading) + ' = ' + str(result))
                 #Listening
                 print(Fore.YELLOW + 'Listening')
@@ -55,14 +75,15 @@ def get_info(language_level, num_exam, students,average_overall,average_reading,
                 aux = str(input())                
                 if not aux:
                     result = 'X'
-                    dict_results['Listening'].append(str(aux) + ' = ' + str(result))
+                    #dict_results['Listening'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Listening'].append(str(aux))
                 else:
                     listening = instance.filter(aux)
                     result = instance.percent_category(listening[0],listening[1])
                     average_test.append(result)
                     avg_listening.append(result)
                 #dict_results['Listening'].append(str(aux))
-                    dict_results['Listening'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Listening'].append(str(aux))
                 #dict_results['Listening'].append(str(listening) + ' = ' + str(result))
                 #Writting
                 print(Fore.BLUE + 'Writing')
@@ -71,14 +92,14 @@ def get_info(language_level, num_exam, students,average_overall,average_reading,
                 aux = str(input())
                 if not aux:
                     result = 'X'
-                    dict_results['Writing'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Writing'].append(str(aux))
                 else:
                     writing = instance.filter(aux)
                     result = instance.percent_category(writing[0],writing[1])
                     average_test.append(result)
                     avg_writing.append(result)
                     #dict_results['Writing'].append(str(aux))
-                    dict_results['Writing'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Writing'].append(str(aux))
                 #dict_results['Writing'].append(str(writing) + ' = ' + str(result))
                 #Speaking
                 print(Fore.GREEN + 'Speaking')
@@ -87,16 +108,16 @@ def get_info(language_level, num_exam, students,average_overall,average_reading,
                 aux = str(input())
                 if not aux:
                     result = 'X'
-                    dict_results['Speaking'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Speaking'].append(str(aux))
                 else:
                     speaking = instance.filter(aux)
                     result = instance.percent_category(speaking[0],speaking[1])
                     average_test.append(result)
                     avg_speaking.append(result)
                 #dict_results['Speaking'].append(aux)
-                    dict_results['Speaking'].append(str(aux) + ' = ' + str(result))
+                    dict_results['Speaking'].append(str(aux))
                 #dict_results['Speaking'].append(str(speaking) + ' = ' + str(result))
-                if language_level[0] == "B2" and language_level[1] == 2:
+                if (language_level[0] == "B2" and language_level[1] == 2) or (language_level[0] == "C1"):
                     #English use
                     print(Fore.WHITE + 'English use')
                     print(underline)
@@ -104,45 +125,34 @@ def get_info(language_level, num_exam, students,average_overall,average_reading,
                     aux = str(input())
                     if not aux:
                         result = 'X'
-                        dict_results['Use_English'].append(str(aux) + ' = ' + str(result))
+                        dict_results['Use_English'].append(str(aux))
                     else:
                         english_use = instance.filter(aux)                    
                         result = instance.percent_category(english_use[0],english_use[1])
                         average_test.append(result)
                         avg_use_english.append(result)
                     #dict_results['English_use'].append(aux)
-                        dict_results['Use_English'].append(str(aux) + ' = ' + str(result))
+                        dict_results['Use_English'].append(str(aux))
                     #dict_results['English_use'].append(str(english_use) + ' = ' + str(result))
                 else: #It is a B1 exam
                     dict_results['Use_English'].append(str('X'))
                 #calculate average and insert in Overall array
                 aux = np.mean(average_test)
-                dict_results['Overall'].append(aux)                
+                dict_results['Overall'].append(np.ceil(aux))                
             average_overall.append(aux)
             average_reading.append(np.mean(avg_reading))
             average_listening.append(np.mean(avg_listening))
             average_writing.append(np.mean(avg_writing))
             average_speaking.append(np.mean(avg_speaking))
-            if language_level[0] == "B2" and language_level[1] == 2:
+            if (language_level[0] == "B2" and language_level[1] == 2) or (language_level[0] == "C1"):
                 average_use_english.append(np.mean(avg_use_english))
             
         #print(dict_results)
-        return dict_results
+        return language_level, dict_results
 
 
 if __name__ == "__main__":
-    language_level = [" "]
-    print("¿Cuántos alumnos tienes?")
-    students = int(input())
-    print('¿Cuántos exámenes quieres evaluar?')
-    num_exam = int(input())
-    while(language_level[0] != "B1" and language_level[0] != "B2"):
-        print("¿Se presentan a B1 o B2?")
-        language_level[0] = str(input())
-        print(language_level[0])
-        if language_level[0] == "B2":
-            print("¿Son alumnos de primer o segundo año? (escribe 1 o 2)")
-            language_level.append(int(input()))
+    
 
     #storing averages
     
@@ -156,23 +166,35 @@ if __name__ == "__main__":
     #Showing info by terminal
     #========================
 
-    dict_notes = get_info(language_level,num_exam,students,averageOverall,averageReading,averageListening,averageSpeaking,averageWriting,averageUseEnglish)    
-
-
+    language_level, dict_notes = get_info(averageOverall,averageReading,averageListening,averageSpeaking,averageWriting,averageUseEnglish)        
+    '''
     document = Document('test.docx')
     docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=1, new_string='Hello')
     document.save('test.docx')
-
     '''
+
     valuetoclean = "np.float64("
+
+    document = Document('test.docx')
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=0, new_string=str(dict_notes['Nombre']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=1, new_string=str(dict_notes['Reading']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=2, new_string=str(dict_notes['Use_English']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=3, new_string=str(dict_notes['Writing']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=4, new_string=str(dict_notes['Listening']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=5, new_string=str(dict_notes['Speaking']).replace('[\'','').replace('\']',''))
+    docxedit.add_text_in_table(document.tables[0], row_num=1, column_num=6, new_string=str(dict_notes['Overall']).replace(valuetoclean,'').replace(')','').replace('[','').replace(']',''))
+
+
+    
+    '''
     fichero = open('Examenes.txt', 'w')
     fichero.write("Estos son los resultados de los examenes o del examen que has insertado:\n")
     fichero.write('\n'.join("{}: {}".format(k, v) for k, v in dict_notes.items()))    
     fichero.write("\n")
-    arrayStudents = dict_notes
     fichero.write("\n")
     fichero.write("Los alumnos han obtenido las siguientes medias por área:\n")
     fichero.write(str(dict_notes['Nombre']))
+    
     fichero.write("\n")
     fichero.write("Reading:")
     fichero.write(str(averageReading).replace(valuetoclean,'').replace(')',''))
@@ -196,3 +218,5 @@ if __name__ == "__main__":
     fichero.write("Fin de las medias por área\n")    
     fichero.close()
     '''
+    document.save('test.docx')
+    
