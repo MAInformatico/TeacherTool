f036c2370238e59fdb3fc2183d6c0166dd0ba58b
mainformatico <magutierrezsistemas@gmail.com>
2025-04-21 20:18:04 +0200

    adding first commit frontend

---
+++ front/teachertool/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <head>
+      <title>Teacher Tool</title>
+    </head>
+    );
+  }
+}
+export default Header;
