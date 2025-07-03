6365063b2c0e4bebf0cd5fdda057e6935c2ee202
Migue <magutierrezsistemas@gmail.com>
2025-07-03 19:51:05 +0200

    updating requirements and execute file

--- execute.sh
+++ execute.sh
@@ -6,4 +6,9 @@ fastapi run fastapi_main.py
 
 
 // If you want to execute the application in local mode, using the terminal or command line interface, you can use the following command:
+// create a virtual environment and install the required packages
+python -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+pip install -r requirements.txt 
+//then run the application
 python main.py
